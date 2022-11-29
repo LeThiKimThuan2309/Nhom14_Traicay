5eb32a4a6d7ff524e761a3fc330a930dbccd9279
BichQuy051307 <quy.ntb.61cntt@ntu.edu.vn>
2022-11-29 23:21:44 +0700

    bai tap bich quy

---
+++ bill.php
@@ -0,0 +1,14 @@
+<?php
+    if(isset($_POST['dongydathang'])&&($_POST['dongydathang'])){
+        //lay thong tin tu from de tao don hang
+
+        //lay thong tin gio hang tu session +id don hang vua tao
+
+        //insert vao gio hang
+        //show confirm don hang
+
+        //unset gio hang session
+    echo "Bạn đã tạo thành công đơn hàng của bạn là:";
+}
+
+?>
