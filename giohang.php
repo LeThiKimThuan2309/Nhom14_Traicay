3e2885b190c956f2bb16bcc6a2fded433b04c16b
BichQuy051307 <quy.ntb.61cntt@ntu.edu.vn>
2022-12-03 14:19:29 +0700

    bai tap moi cua quy

--- giohang.php
+++ giohang.php
@@ -1,195 +1,82 @@
+
+<h2 align="center" ><a style="color:#000; text-decoration:none;">GIỎ HÀNG CỦA BẠN</a></h2>
 <?php
     // session_start();
-
-    include "thuvien.php";
-    if(!isset($_SESSION['giohang'])) $_SESSION['giohang']=[];
-    //làm rỗng giỏ hàng
-    if(isset($_GET['delgiohang'])&&($_GET['delgiohang']==1)) unset($_SESSION['giohang']);
-    //xóa sp trong giỏ hàng
-    if(isset($_GET['delid'])&&($_GET['delid']>=0)){
-       array_splice($_SESSION['giohang'],$_GET['delid'],1);
-    }
-    //lấy dữ liệu từ form
-    if(isset($_POST['addgiohang'])&&($_POST['addgiohang'])){
-        $hinh=$_POST['hinh'];
-        $tensp=$_POST['tensp'];
-        $gia=$_POST['gia'];
-        $soluong=$_POST['soluong'];
-
-        //kiem tra sp co trong gio hang hay khong?
-
-        $fl=0; //kiem tra sp co trung trong gio hang khong?
-
-        for ($i=0; $i < sizeof($_SESSION['giohang']); $i++) { 
-            
-            if($_SESSION['giohang'][$i][1]==$tensp){
-                $fl=1;
-                $soluongnew=$soluong+$_SESSION['giohang'][$i][3];
-                $_SESSION['giohang'][$i][3]=$soluongnew;
-                break;
-
-            }
-            
-        }
-        //neu khong trung sp trong gio hang thi them moi
-        if($fl==0){
-            //them moi sp vao gio hang
-            $sp=[$hinh,$tensp,$gia,$soluong];
-            $_SESSION['giohang'][]=$sp;
-        }
-
-       // var_dump($_SESSION['giohang']);
-    }
-
-    
-
-
 ?>
+<?php 
+    if(isset($_SESSION['cart'])){
+        // print_r($_SESSION['cart']);
+    }
+?>
+<style>
+    table{
+        width:100%;
+    }
+    table, th, td {
+        border:1px solid black;
+        /* margin-left:100px; */
+        text-align:center;
+    }
+    img{
+        width:100px;
+    }
+</style>
+<table >
+  <tr>
+    <th>Id</th>
+    <th>Tên trái cây</th>
+    <th>Hình ảnh</th>
+    <th>Số lượng</th>
+    <th>Giá sản phẩm</th>
+    <th>Thành tiền</th>
+    <th>Xử lý</th>
 
-<!DOCTYPE html>
-<html lang="en">
+  </tr>
+  <?php
+    if(isset($_SESSION['cart'])){
+        $i=0;
+        $tongtien=0;
+        foreach($_SESSION['cart'] as $cart_item){
+            $thanhtien=$cart_item['amount']*$cart_item['price'];
+            $tongtien+=$thanhtien;
+            $i++;
+?>
+        <tr style="text-decoration:none;">
+            <td ><?php echo $i; ?></td>
+            <td><?php echo $cart_item['title'];?></td>
+            <td><img src="../BaiTapLon/Admin/The_Loai/upload/<?php echo $cart_item['thumbnail'];?>"></td>
+            <td>
+                <a href="main/themgiohang.php?cong=<?php echo $cart_item['id']?>"><i class="fa fa-plus fa-style"></i></a>
+                <?php echo $cart_item['amount'];?>
+                <a href="main/themgiohang.php?tru=<?php echo $cart_item['id']?>"><i class="fa fa-minus fa-style"></i></a>
+            </td>
+            <td><?php echo $cart_item['price'];?></td>  
+            <td><?php echo number_format ($thanhtien,0,',','.'). 'vnđ';?></td>   
+            <td><a href="main/themgiohang.php?xoa=<?php echo $cart_item['id']?>">Xóa</a></td>   
+        </tr>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Shopping  | View Gio Hang</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <div class="boxcenter">
-         <div class="row mb header">
-            <h1> SIÊU THỊ TRÁI CÂY</h1>
-        </div> 
-        <div class="row mb menu">
-            <ul>
-                <li><a href="trangchu.php">Trang chủ</a></li>
-                <li><a href="cart.php">Giỏ hàng</a></li>
-                <li><a href="#">Liên hệ</a></li>
-                <li><a href="#">Góp ý</a></li>
-                <li><a href="#">Hỏi đáp</a></li>
-            </ul> 
-        <div class="row mb ">
-            <div class="boxtrai mr" id="hoadon">
-                <from action="bill.php" method="post">
-                <div class="row" >
-                    <h1>THÔNG TIN NHẬN HÀNG</h1>
-                    <table class="thongtinnhanhang">
-                        <tr>
-                            <td width="20%">Họ tên</td>
-                            <td><input type="text" name="hoten" required></td>
-                        </tr>
-                        <tr>
-                            <td>Địa chỉ</td>
-                            <td><input type="text" name="diachi" required></td>
-                        </tr>
-                        <tr>
-                            <td>Điện thoại</td>
-                            <td><input type="text" name="dienthoai" required></td>
-                        </tr>
-                        <tr>
-                            <td>Email</td>
-                            <td><input type="text" name="email" required></td>
-                        </tr>
-                    </table>
-                </div>
-                <div class="row mb">
-                    <h1>GIỎ HÀNG</h1>
-                    <table>
-                        <tr>
-                            <th>STT</th>
-                            <th>Hình</th>
-                            <th>Tên sản phẩm</th>
-                            <th>Đơn giá</th>
-                            <th>Số lượng</th>
-                            <th>Thành tiền ($)</th>
-                            <th>Xóa</th>
-                        </tr>
-                        <?php showgiohang(); ?>
-                        <!-- <tr>
-                            <td>1</td>
-                            <td><img src="images/1.jpg" alt=""></td>
-                            <td>Trái cây</td>
-                            <td>10</td>
-                            <td>1</td>
-                            <td>
-                                <div>10</div>
-                            </td>
-                        </tr> -->
-                        <!-- <tr>
-                            <th colspan="5">Tổng đơn hàng</th>
-                            <th>
-                                <div>10</div>
-                            </th>
+<?php
+        }
+?>
+    <tr>
+         <td colspan="8" align="center">
+            <p style="float:center;">Tổng tiền <?php echo number_format($tongtien,0,',','.').'vnđ'?></p></br>
+            <p style="float:right;"><a href="">Xóa tất cả</a></p>
 
-                        </tr> -->
-                    </table>
-                </div>
-                <div class="row mb">
-                    <input type="submit" value="ĐỒNG Ý ĐẶT HÀNG" name="dongydathang">
-                    <a href="cart.php?delcart=1"><input type="button" value="XÓA GIỎ HÀNG"></a>
-                    <a href="index.php"><input type="button" value="TIẾP TỤC ĐẶT HÀNG"></a>
-                </div>
-                </from>
-            </div>
-            <div class="boxphai">
-                <div class="row mb ">
-                    <div class="boxtitle">TÀI KHOẢN</div>
-                    <div class="boxcontent formtaikhoan">
-                        <form action="#" method="post">
-                            <div class="row mb10">
-                                Tên đăng nhập<br>
-                                <input type="text" name="user">
-                            </div>
-                            <div class="row mb10">
-                                Mật khẩu<br>
+            <div style="clear:both;"></div>
+            
+                <p><a href="main/thanhtoan.php">Đặt hàng</p>
+         </td>   
+    </tr>
+<?php
+    }else{
+?>
+    <tr>
+         <td colspan="8"><p>Giỏ hàng trống</p></td>
+    </tr>
+<?php
+}
+?>
+</table>
 
-                                <input type="password" name="pass">
-                            </div>
-                            <div class="row mb10">
-                                <input type="checkbox" name=""> Ghi nhớ tài khoản?</div>
-                            <div class="row mb10">
-                                <input type="submit" value="Đăng nhập">
-                            </div>
-                        </form>
-                        <li>
-                            <a href="#">Quên mật khẩu</a>
-                        </li>
-                        <li>
-                            <a href="#">Đăng ký thành viên</a>
-                        </li>
-                    </div>
-                </div>
-                <!-- <div class="row mb">
-                    <div class="boxtitle">DANH MỤC</div>
-                    <div class="boxcontent2 menudoc">
-                        <ul>
-                            <li>
-                                <a href="#">Trái cây miền Bắc</a>
-                            </li>
-                            <li>
-                                <a href="#">Trái cây miền Nam</a>
-                            </li>
-                            <li>
-                                <a href="#">Trái cây miền Trung</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="boxfooter searbox">
-                        <form action="#" method="post">
-                            <input type="text" name="" id="">
-                        </form>
-                    </div>
-                </div> -->
-                
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- <div class="row mb footer">
-        </div> -->
-    </div>
 
-</body>
-
-</html>
