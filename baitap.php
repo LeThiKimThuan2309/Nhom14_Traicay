e469fcf16979a30a7515c14400bc7b755aeb6f27
ngochung1010 <118986885+ngochung1010@users.noreply.github.com>
2022-12-03 21:55:54 +0700

    Add files via upload

---
+++ baitap.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bài tập</title>
+</head>
+<style>
+    table,tr,td{
+       text-align: center;
+    }
+</style>
+<body>
+   
+    <table align='center' width='70%'border='1' callpadding='2' style='border-collapse:collapse'>
+    
+    <tr><td colspan='3' align='center'style='background-color:#458b74'><p align='center'><font size='6' color='white'>BÀI TẬP CÁ NHÂN</font></p></td></tr>
+
+        
+
+    <tr>
+            <th>PHP && FORM</th>
+            <th>MẢNG, CHUỖI & HÀM</th>
+            <th>KẾT HỢP PHP & MYSQL</th>
+        </tr>
+        <tr>
+            <td><a href="../BT_form/bai1_form.php" style="color:#444444;">Bài tập 1</a></td>
+            <td><a href="../BT_mang_chuoi_ham/BT1_arr.php" style="color:#444444;">Bài tập 1</a></td>
+            <td><a href="../BT_SQL/bai2_1SQL.php" style="color:#444444;">Bài tập 2.1</a></td>
+        </tr>
+        <tr>
+            <td><a href="../BT_form/bai1_form.php" style="color:#444444;">Bài tập 2</a></td>
+            <td><a href="../BT_mang_chuoi_ham/BT2_arr.php" style="color:#444444;">Bài tập 2</a></td>
+            <td><a href="../BT_SQL/bai2_2SQL.php" style="color:#444444;">Bài tập 2.2</a></td>
+        </tr>
+        <tr>
+            <td><a href="../BT_form/bai3_form.php" style="color:#444444;">Bài tập 3</a></td>
+            <td><a href="../BT_mang_chuoi_ham/BT3_func.php" style="color:#444444;">Bài tập 3</a></td>
+            <td><a href="../BT_SQL/bai2_3SQL.php" style="color:#444444;">Bài tập 2.3</a></td>
+        </tr>
+        <tr>
+            <td><a href="../BT_form/bai4_form.php" style="color:#444444;">Bài tập 4</a></td>
+            <td><a href="../BT_mang_chuoi_ham/BT4_func.php" style="color:#444444;">Bài tập 4</a></td>
+            <td><a href="../BT_SQL/bai2_4SQL.php" style="color:#444444;">Bài tập 2.4</a></td>
+        </tr>
+        <tr>
+            <td><a href="../BT_form/bai5_form.php" style="color:#444444;">Bài tập 5</a></td>
+            <td><a href="../BT_mang_chuoi_ham/BT5_func.php" style="color:#444444;">Bài tập 5</a></td>
+            <td><a href="../BT_SQL/bai2_5SQL.php" style="color:#444444;">Bài tập 2.5</a></td>
+        </tr>
+        <tr>
+            <td><a href="../BT_form/bai6_7kq_form.php" style="color:#444444;">Bài tập 6_7</a></td>
+            <td><a href="../BT_mang_chuoi_ham/BT6_func.php" style="color:#444444;">Bài tập 6</a></td>
+            <td><a href="../BT_SQL/bai2_6SQL.php" style="color:#444444;">Bài tập 2.6</a></td>
+        </tr>
+        <tr>
+            <td><a href="../BT_form/Config_8.php" style="color:#444444;">Bài tập 8</a></td>
+            <td><a href="../BT_mang_chuoi_ham/BT7_func.php" style="color:#444444;">Bài tập 7</a></td>
+            <td><a href="../BT_SQL/bai2_7SQL.php" style="color:#444444;">Bài tập 2.7</a></td>
+        </tr>
+        <tr>
+            <td><a href="../BT_form/Config_8.php" style="color:#444444;"></a></td>
+            <td></td>
+            <td><a href="../BT_SQL/bai2_8SQL.php" style="color:#444444;">Bài tập 2.8</a></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td><a href="../BT_SQL/bai2_9SQL.php" style="color:#444444;">Bài tập 2.9</a></td>
+        </tr>
+        <tr>
+            <td> </td>
+            <td></td>
+            <td><a href="../BT_SQL/bai2_10SQL.php" style="color:#444444;">Bài tập 2.10</a></td>
+        </tr>
+        <tr>
+            <td  colspan='3' ><a href='javascript:window.history.back(-1);'>Trang chủ</a></td>
+        </tr>
+    </table>
+    
+    
+</body>
+</html>
