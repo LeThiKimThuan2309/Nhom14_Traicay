daf59999f4e05f0907d7e6a9615410624a13ecd6
Lê Thị Kim Thuận <110155058+LeThiKimThuan2309@users.noreply.github.com>
2022-12-03 21:51:49 +0700

    Update thanhtoans.php

--- thanhtoans.php
+++ thanhtoans.php
@@ -1,24 +1,29 @@
-
 <?php 
+    include("../../BaiTapLon/db/config.php");
     session_start();
-    include("../../sql/config.php");   
-    // if(isset($_SESSION['username']))
+  
+    if(isset($_SESSION['username']) && isset($_SESSION['id_khachhang']) )
     {   
-        $id_khachhang = $_SESSION['id_khachhang'];
+        $khachhang = $_SESSION['username'];
         $code_order = rand(0,9999);
-        $insret_cart = "INSERT INTO tbl_cart(id_khachhang,code_card,card_status) VALUE('$id_khachhang','$code_order',1)";
-        $cart_query = mysqli_query($insret_cart);
+        $insret_cart = "INSERT INTO tbl_cart(khachhang,code_cart,cart_status) VALUES('$khachhang','$code_order',1)";
+        $cart_query = mysqli_query($conn,$insret_cart);
         if($insret_cart){
             //them gio hang chi tiet
             foreach($_SESSION['cart'] as $key => $value){
             $id_sanpham = $value['id'];
             $amount = $value['amount'];
-            $insert_order_details =  "INSERT INTO tbl_cart_details(id_sanpham,code_card,soluong) VALUE ('$id_sanpham','$code_order','$amount')";
-            mysqli_query($mysqli,$insert_order_details);
+            $insert_order_details =  "INSERT INTO tbl_cart_details(id_sanpham,code_cart,soluong) VALUES ('$id_sanpham','$code_order','$amount')";
+            mysqli_query($conn,$insert_order_details);
             }
         }
+        unset ($_SESSION['cart']);
+        echo "<h2 style='text-align:center;color:red; text-size:50px;'>Bạn đã đặt hàng thành công!!!</h2>";
+        echo"<a href='../index.php' style='color:#444444;'>Trở về</a>";
     }
-    // echo"<a href='dangnhap.php' style='color:#444444;'>Đăng nhập</a>";
-    unset ($_SESSION['cart']);
+    else
+  
+    echo"<a href='../dangnhap.php' style='color:#444444;'>Đăng nhập</a>";
+    
  
 ?>
